82550f52d992f3eae4d4fe566e965e4369b470b2
Austin Joe McGill <austin-12_@hotmail.com>
2024-03-05 16:46:52 -0500

    commit 2
    3-5-24 changed location

--- InventoryManagementSystem/obj/Debug/net8.0/InventoryManagementSystem.AssemblyInfo.cs
+++ InventoryManagementSystem/obj/Debug/net8.0/InventoryManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+883e1980f3ac35f58412896beed35ceec03aeea7")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
